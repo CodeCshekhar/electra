ac42e56f5d166f90ae7744da8e2a01833e0e7e87
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-07-30 21:14:14 +0530

    commits

--- src/com/Electra/model/Customer.java
+++ src/com/Electra/model/Customer.java
@@ -3,20 +3,21 @@ package com.Electra.model;
 
 public class Customer {
     private Long id;
-    private final String name;
-    private final String email;
-    private final Address address;
+    private  String name;
+    private  String email;
+
 
     // Constructors, getters, setters, and other methods
 
 
-    public Customer(Long id, String name, String email, Address address) {
+    public Customer(Long id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.address = address;
+
     }
     // Getters and Setters (omitted for brevity)
+
 
     public Long getId() {
         return id;
@@ -26,13 +27,28 @@ public class Customer {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return "Customer{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
-                ", address=" + address +
                 '}';
     }
 }
